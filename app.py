46724af4517569090ea36f05378ef14a86e3bbd6
prince <prince.sethi@trantorinc.com>
2019-12-05 14:23:30 +0530

    update

--- app.py
+++ app.py
@@ -25,17 +25,17 @@ class Prediction(Resource):						#Prediction class inherits Resource class
     )
 #----function which receives post request from client app and returns json after processing.
 	def post(self):
-		input_data =  Prediction.parser.parse_args()		#input_data is the data sent by client app in json format
-		width = input_data['width']							#extracts width from input data
-		length = input_data['length']						#extracts length from input data
-		height = input_data['height']						#extracts height from input data
+		input_data =  Prediction.parser.parse_args()		#parser receives data from client app and converts into python dictionary
+		width = input_data['width']							
+		length = input_data['length']						
+		height = input_data['height']						
 
 		model_instance = model.Regression(width, length)			#creates instance of Regression class and run constructor of class
 		predicted_value = float(model_instance.predict(height))		#calls the predict function of Regression class and saves the return data to predicted_value variable
 		
-		return {"price": predicted_value}				#returns processed data to client app
+		return {"price": predicted_value}				#returns processed data to client app with default response code 200 OK
 
-api.add_resource(Prediction, '/')			#To add a resource Prediction and define its end point
+api.add_resource(Prediction, '/')			#To add a resource Prediction and define its Endpoint
 
 if __name__ == '__main__':					#To call app.run only on local system
     app.run(port=5000, debug=True)  		# debug=True provides better error messages
